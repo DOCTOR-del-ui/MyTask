d26100cc60140263899575060d25c813e353e489
darkdormammu <69586674+DOCTOR-del-ui@users.noreply.github.com>
2023-12-10 21:50:12 +0800

    Update fid.py

--- score/fid.py
+++ score/fid.py
@@ -97,7 +97,7 @@ def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6,
         diff = mu1 - mu2
         # Run 50 itrs of newton-schulz to get the matrix sqrt of
         # sigma1 dot sigma2
-        covmean = sqrt_newton_schulz(sigma1.mm(sigma2).unsqueeze(0), 50)
+        covmean = sqrt_newton_schulz(sigma1.mm(sigma2).unsqueeze(0), 25)
         if torch.any(torch.isnan(covmean)):
             return float('nan')
         covmean = covmean.squeeze()
